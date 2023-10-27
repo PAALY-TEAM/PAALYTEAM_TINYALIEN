1dca6bb72928b12a500746556b75745a786ef126
NoaTheSoap <noa.sjoset@gmail.com>
2023-10-27 15:48:43 +0200

    Lava commit

---
+++ Assets/Scripts/Minigame/FloorIsLava/Lava.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava : MonoBehaviour
+{
+    float LowestPoint = 3.5f;
+    float LavaHeight;
+    bool RaiseLowerToggle = true;
+    [SerializeField] float LavaSpeed = 0.2f;
+    [SerializeField] GameObject LavaHeighObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Finds Y difference between Lava and lavaHeightObject to find nummber to scale lava to
+        LavaHeight = (LavaHeighObject.transform.position.y - transform.position.y)*2;
+        Debug.Log(LavaHeight);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // If lava is at top point lower
+        if(transform.localScale.y >= LavaHeight)
+        {
+            RaiseLowerToggle = false;
+            
+        }
+
+        //If lava is at bot raise
+        if (transform.localScale.y <= LowestPoint)
+        {
+            RaiseLowerToggle = true;
+        }
+
+   
+
+        raiseLower();
+
+    }
+    // Die on hit with lava
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Dead");
+        }
+
+    }
+
+    void raiseLower()
+    {
+        //Raises the lava
+        if (transform.localScale.y < LavaHeight && RaiseLowerToggle)
+        {
+            transform.localScale += new Vector3(0, LavaSpeed * Time.deltaTime, 0);
+        }
+        //Lowers the lava
+        if (!RaiseLowerToggle)
+        {
+            transform.localScale -= new Vector3(0, LavaSpeed * Time.deltaTime, 0);
+        }
+
+    }
+
+
+
+
+}
