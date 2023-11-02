218af0a545ff750602144d4bebd165718394e2ea
MarzLars <lars_gustav@hotmail.com>
2023-11-02 12:03:35 +0100

    Door interaction script now could check for color of player

--- Assets/Scripts/Interaction/DoorTriggerInteraction.cs
+++ Assets/Scripts/Interaction/DoorTriggerInteraction.cs
@@ -14,6 +14,9 @@ public class DoorTriggerInteraction : TriggerInteractionBase
         Five,
         // Add more if needed
     }
+    [Header("Door Condition")]
+    //Should the door look for a specific material on the player?
+    [SerializeField] private bool useSharedMaterialCondition = false;
     [Header("DoorToSpawnTo")]
     // Which door in new scene are we going to go into
     [SerializeField] private DoorToSpawnAt doorToSpawnTo; 
@@ -22,15 +25,23 @@ public class DoorTriggerInteraction : TriggerInteractionBase
     [Space(10f)]
     [Header("THIS DOOR")]
     public DoorToSpawnAt currentDoorPosition;
-
+    
     public override void Interact()
     {
         //GameObject.FindGameObjectWithTag("Player").GetComponent<ItemManager>().hintText.SetActive(true);
         GameObject player = GameObject.FindGameObjectWithTag("Player");
+        // If player has item manager, set hint text to active
         if (player.TryGetComponent<ItemManager>(out ItemManager itemManager))
         {
             itemManager.hintText.SetActive(true);
         }
+        // If player is not allowed to interact with this door, return
+        if (!CanInteractWithDoor(player))
+        {
+            Debug.Log("Player is not allowed to interact with this door.");
+            return;
+        }
+        // If scene to load is not set, return
         if (sceneToLoad == null || doorToSpawnTo == DoorToSpawnAt.None)
         {
             Debug.Log("Scene to load is not set. Interaction with door is disabled.");
@@ -38,5 +49,18 @@ public class DoorTriggerInteraction : TriggerInteractionBase
         }
         // Load new scene
         SceneSwapManager.SwapSceneFromDoorUse(sceneToLoad, doorToSpawnTo);
+    }
+    // Check if player can interact with door
+    private bool CanInteractWithDoor(GameObject player)
+    {
+        if (!useSharedMaterialCondition)
+        {
+            return true;
+        }
+
+        Material playerMaterial = player.GetComponent<Renderer>().sharedMaterial;
+        Material doorMaterial = gameObject.GetComponent<Renderer>().sharedMaterial;
+    
+        return playerMaterial == doorMaterial;
     }
 }
--- Assets/Scripts/SceneLogic/Initializer.cs
+++ Assets/Scripts/SceneLogic/Initializer.cs
@@ -1,9 +1,14 @@
-public interface INitializer
+using UnityEngine;
+public interface Initializer
 {
-    /*  [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    /*
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
     public static void Execute()
     {
         Debug.Log("Loaded by Persist Objects from the Initializer script");
-        Object.DontDestroyOnLoad(Object.Instantiate(Resources.Load("PERSISTOBJECTS")));
-    }*/
+        
+        // Load the player
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+    }
+    */
 }
